5eb4dc99af6f86597a56a0b921c2af7219286487
Alexander Quatrini <alexanderquatrini@gmail.com>
2022-09-19 17:37:04 -0500

    Displays game list

--- ClientApp/src/app/game-list/game-list.component.ts
+++ ClientApp/src/app/game-list/game-list.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
+import { IGameList } from 'src/models/IGameList.model';
 import { Constants } from '../util/constants.util';
 
 @Component({
@@ -15,15 +16,53 @@ export class GameListComponent implements OnInit {
   @Input()
   sessionID: string = "";
 
+  @Input()
+  personaName?: string = "";
+
   API_PORT = Constants.apiPort;
   API_URL = Constants.apiUrl;
+
+  numberOfPages: number = 0;
+  gameListObject: IGameList = {};
+  noGameList: boolean = false;
+  gamesPerPage: number = 15;
+
+  currentGameList: IGameList = {};
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     var n = this.steamID.lastIndexOf('/');
     var IDSub = this.steamID.substring(n + 1);
-    this.http.post(this.API_URL+":"+this.API_PORT+"/api/steam/getgamelist", {steamID: IDSub, sessionID: this.sessionID}).toPromise().then(data => console.log(data));
+    this.http.post<IGetGameListResponse>(this.API_URL+":"+this.API_PORT+"/api/steam/getgamelist", {steamID: IDSub, sessionID: this.sessionID}).toPromise().then(data => {
+      this.gameListObject = data.response;
+      console.log(this.gameListObject);
+      this.numberOfPages =  this.gameListObject.game_count == undefined ? 0 : Math.trunc(this.gameListObject.game_count / this.gamesPerPage) + 1;
+      this.currentGameList = this.getPageOfItems(1);
+    }).catch(error => {
+      console.log(error);
+      this.noGameList = true;
+    });
   }
 
+  getPageOfItems(pageNumber: number): IGameList{
+    if(this.gameListObject.games != undefined){
+
+    var length = this.gameListObject.games.length;
+    var items = pageNumber * 15 - 1 >= length ? 
+    this.gameListObject.games.slice((pageNumber - 1) * 15) : 
+    this.gameListObject.games.slice((pageNumber - 1) * 15, pageNumber * 15 - 1);
+
+    return {game_count: length, games: items};
+    }
+
+    else{
+      return {};
+    }
+  }
+
+}
+
+interface IGetGameListResponse{
+  response: IGameList;
 }
---
+++ ClientApp/src/models/IGameList.model.ts
@@ -0,0 +1,10 @@
+export interface IGameList{
+    game_count?: number;
+    games?: Array<{
+        appid?: string,
+        img_icon_url?: string,
+        name?: string,
+        playtime_forever?: string,
+
+    }>;
+} 
